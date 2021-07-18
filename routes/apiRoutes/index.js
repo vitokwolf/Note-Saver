21a76fb77b2ac205426576fb0b163874cf63a8c7
Victor Lupascu <sinajeen@gmail.com>
2021-07-18 14:36:56 -0500

    add remove note function

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
-const { db } = require('../../data/db.json');
+const { notes } = require('../../data/db.json');
+const {createNewNote, deleteNote} = require('../../lib/notes');
 
+// route to render 'notes' data
 router.get('/notes', (req, res) => {
-    let results = db;
-    res.json(results);
+    res.json(notes);
 });
 
+// route to add new data to 'notes'
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+
+});
+
+// deletes data from the file
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const removeNote = deleteNote(id, notes);
+    res.json(removeNote);
+})
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
+// route to render functional page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+// route to render landing page
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });

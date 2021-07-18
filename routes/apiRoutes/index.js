c9a1351c9f52d66d3e358c9e8141d914a1cda96c
Victor Lupascu <sinajeen@gmail.com>
2021-07-18 16:59:49 -0500

    update create and delete functions to edit id accordingly

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -9,15 +9,13 @@ router.get('/notes', (req, res) => {
 
 // route to add new data to 'notes'
 router.post('/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
     const note = createNewNote(req.body, notes);
     res.json(note);
 
 });
 
 // deletes data from the file
-router.delete('/notes', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     const id = req.params.id;
     const removeNote = deleteNote(id, notes);
     res.json(removeNote);
